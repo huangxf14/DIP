23ff3e0b07b6053d7669f7eff1b6ac431dfaed7f
huangxf14 <464271301@qq.com>
2017-05-19 15:43:10 +0800

    add match

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,21 @@
 #include "dip.hpp"
+
+vector<Point2i> match(vector<Point2i> &point1,vector<Point2i> &point2)
+{
+	vector<Point2i> ans;
+	for (int i=0;i<point1.size();++i)
+	{
+		int min=10000000,point=0,temp;
+		for (int j=0;j<point2.size();++j)
+		{
+			temp=norm(point1[i]-point2[j]);
+			if (min>temp)
+			{
+				min=temp;
+				point=j;
+			};
+		};
+		ans.push_back(Point2i(i,point));
+	};
+	return ans;
+};
